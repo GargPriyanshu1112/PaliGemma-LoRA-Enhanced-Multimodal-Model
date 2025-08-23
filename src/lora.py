ac70c7c74a5743ac11cbbb050f0baa4c1c19aafb
GargPriyanshu1112 <priyanshu273141@gmail.com>
2025-08-23 12:44:45 +0530

    parameterized lora for better training control

--- src/lora.py
+++ src/lora.py
@@ -1,8 +1,10 @@
+# TODO: Handle weight shape consistency when fan_in_fan_out=True. 
+# TODO: Handle dropout
+
 import math
 import torch
 import torch.nn.utils.parametrize as parameterize
 from torch import nn
-from functools import partial
 
 
 class LoraParameterization(nn.Module):
@@ -12,8 +14,8 @@ class LoraParameterization(nn.Module):
         out_features,
         fan_in_fan_out=False,
         r=0,
-        lora_alpha=1,
-        lora_dropout_p=0.0,
+        lora_alpha=32,
+        lora_dropout_p=0.05,
         **kwargs
     ):
         super().__init__()
@@ -21,7 +23,7 @@ class LoraParameterization(nn.Module):
         if self.r > 0:
             self.fan_in_fan_out = fan_in_fan_out
             self.scaling = lora_alpha / r
-            self.lora_dropout = nn.Dropout(lora_dropout_p) if lora_dropout_p > 0.0 else lambda x: x
+            self.lora_dropout = nn.Dropout(lora_dropout_p) if lora_dropout_p > 0.0 else (lambda x: x)
             self.swap_fn = (lambda x: (x[1], x[0])) if self.fan_in_fan_out else (lambda x: x)
             self.lora_A = nn.Parameter(
                 torch.zeros(self.swap_fn((r, in_features))), 
@@ -41,13 +43,13 @@ class LoraParameterization(nn.Module):
         return X
     
     def lora_forward(self, X):
-        return X + torch.matmul(*self.swap_fn(self.lora_B, self.lora_A)).view(X.shape) * self.scaling
+        return X + torch.matmul(*self.swap_fn((self.lora_B, self.lora_A))) * self.scaling
     
     def forward(self, X):
         return self.forward_fn(X)   
 
     @classmethod
-    def add_lora_weights_to_linear(cls, layer, r=4, lora_alpha=1, lora_dropout_p=0.0):
+    def add_lora_weights_to_linear(cls, layer, r=0, lora_alpha=32, lora_dropout_p=0.05):
         out_features, in_features = layer.weight.shape
         return cls(
             in_features, out_features, fan_in_fan_out=False, r=r, lora_alpha=lora_alpha, lora_dropout_p=lora_dropout_p
@@ -56,21 +58,25 @@ class LoraParameterization(nn.Module):
 
 base_lora_config = {
     nn.Linear: {
-        "weight": partial(LoraParameterization.add_lora_weights_to_linear, r=4)
+        "weight": LoraParameterization.add_lora_weights_to_linear
     }
 }
 
-def add_lora_weights(layer, r):
+def add_lora_weights(layer, r=0, lora_alpha=32, lora_dropout_p=0.05):
     for attr, parameterization_fn in base_lora_config[type(layer)].items():
         # When accessing module.weight, the layer will return the parametrized version,
         # (parametrization_fn(module.weight)). If the original tensor requires a gradient,
         # the backward pass will differentiate through parametrization_fn, and the 
         # optimizer will update the tensor accordingly.
         parameterize.register_parametrization(
-            layer, attr, parameterization_fn(layer, r=r)
+            layer, 
+            attr, 
+            parameterization_fn(
+                layer, r, lora_alpha, lora_dropout_p
+            )
         )
         
-def attatch_lora_to_layers(model, target_modules, r):
+def attach_lora_to_layers(model, target_modules, r=0, lora_alpha=32, lora_dropout_p=0.05):
     for name, layer in model.named_modules():
         if any([t in name for t in target_modules]) and (type(layer) in base_lora_config):
-            add_lora_weights(layer, r) # in-place
+            add_lora_weights(layer, r, lora_alpha, lora_dropout_p) # in-place
